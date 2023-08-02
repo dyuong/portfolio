af44b28eee6ccfda40f14a0d9d34e39ddb00f101
dyuong <daniel.yuong.1996@gmail.com>
2023-08-02 20:47:34 +1000

    many changes to abc

--- js/nurture.js
+++ js/nurture.js
@@ -1,60 +1,10 @@
 $(document).ready(function() {
     $("#skip").on("click", function() {
-      var target = $(".final-prototype").offset().top;
-      var offset = target - (target * 0.01);
-      $("html, body").animate({
-        scrollTop: offset
-      }, 100);
-    });
-  });
-
-
-$(document).ready(function() {
-    $("#one").on("click", function() {
-      var target = $(".research").offset().top;
-      var offset = target - (target * 0.05);
-      $("html, body").animate({
-        scrollTop: offset
-      }, 100);
-    });
-  });
-
-$(document).ready(function() {
-    $("#two").on("click", function() {
-      var target = $(".define").offset().top;
-      var offset = target - (target * 0.03);
-      $("html, body").animate({
-        scrollTop: offset
-      }, 100);
-    });
-  });
-
-$(document).ready(function() {
-    $("#three").on("click", function() {
-      var target = $(".ideate").offset().top;
-      var offset = target - (target * 0.016);
-      $("html, body").animate({
-        scrollTop: offset
-      }, 100);
-    });
-  });
-
-  $(document).ready(function() {
-    $("#four").on("click", function() {
       var target = $(".prototype").offset().top;
-      var offset = target - (target * 0.013);
+      var offset = target - (target * 0.005);
       $("html, body").animate({
         scrollTop: offset
       }, 100);
     });
   });
 
-  $(document).ready(function() {
-    $("#five").on("click", function() {
-      var target = $(".prototype").offset().top;
-      var offset = target - (target * 0.013);
-      $("html, body").animate({
-        scrollTop: offset
-      }, 100);
-    });
-  });
